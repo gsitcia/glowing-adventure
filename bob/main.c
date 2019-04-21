e1275493d9357234aee853a14cfd04dd8f28a053
Walter Mitty <gsitcia@gmail.com>
2019-04-21 11:35:51 -0500

    Update main.c

--- bob/main.c
+++ bob/main.c
@@ -1,6 +1,12 @@
 #include <libndls.h>
+#include <nspireio/nspireio.h>
 
 int main(void) {
 	assert_ndless_rev(801);
+	nio_console csl;
+	nio_init(&csl,NIO_MAX_COLS,NIO_MAX_ROWS,0,0,NIO_COLOR_WHITE,NIO_COLOR_BLACK,true);
+	nio_fputs("Hello World!",&csl);
+	nio_free(&csl);
+	wait_key_pressed();
 	return 0;
 }
