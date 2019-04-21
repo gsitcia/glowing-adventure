b6a14125d0dac601ac0ee9a686d302edc03a2e78
Walter Mitty <gsitcia@gmail.com>
2019-04-21 12:43:27 -0500

    Update main2.c

--- bob/main2.c
+++ bob/main2.c
@@ -7,15 +7,23 @@
 nio_console csl;
 
 int fancyputs(char *s) {
-    return 0;
+	nio_clear(&csl);
+	nio_printf("%s",s);
+	return 0;
+}
+
+int register(void) {
+	fancyputs("Hello, World!");
+	return 0;
 }
 
 int main(void) {
 	assert_ndless_rev(801);
+	nl_no_scr_redraw();
 	nio_init(&csl,NIO_MAX_COLS,1,0,0,NIO_COLOR_WHITE,NIO_COLOR_BLACK,true);
-    nio_set_default(&csl);
-    nio_fflush(&csl);
+	nio_set_default(&csl);
+	nio_fflush(&csl);
+	register();
 	nio_free(&csl);
-	wait_key_pressed();
 	return 0;
 }
