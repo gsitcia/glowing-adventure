79dbfc1404938ce4c223376536214bcf86893f1b
Walter Mitty <gsitcia@gmail.com>
2019-04-21 18:14:06 +0000

    compiled and also fixes

--- joe/main.c
+++ joe/main.c
@@ -1,10 +1,23 @@
-#include <os.h>
 #include <usbdi.h>
 #include <usb.h>
 #include <libndls.h>
 #include <nspireio/nspireio.h>
+#include <syscall-list.h>
 
 nio_console csl;
+
+static inline int wa_syscall(int nr)
+{
+	register int r0 asm("r0");
+
+	asm volatile(
+		"swi %[nr]\n"
+		: "=r" (r0)
+		: [nr] "i" (nr)
+		: "memory", "r1", "r2", "r3", "r4", "r12", "lr");
+
+	return r0;
+}
 
 int fancyputs(char *s) {
 	nio_clear(&csl);
@@ -12,18 +25,19 @@ int fancyputs(char *s) {
 	return 0;
 }
 
-int register(void) {
-	fancyputs("Hello, World!");
+int thing_register(void) {
+	fancyputs((char*)"Hello, World!");
 	return 0;
 }
 
 int main(void) {
 	assert_ndless_rev(801);
-	nl_no_scr_redraw();
-	nio_init(&csl,NIO_MAX_COLS,1,0,0,NIO_COLOR_WHITE,NIO_COLOR_BLACK,true);
+	//nl_no_scr_redraw(); // is what I wish I could do
+    wa_syscall(e_nl_no_scr_redraw);
+	nio_init(&csl,NIO_MAX_COLS,1,0,0,NIO_COLOR_WHITE,NIO_COLOR_BLACK,TRUE);
 	nio_set_default(&csl);
 	nio_fflush(&csl);
-	register();
+	thing_register();
 	nio_free(&csl);
 	return 0;
 }
