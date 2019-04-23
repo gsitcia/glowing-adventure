c5776d530ef3d0ed42d7a75879d329e212ec7532
Walter Mitty <gsitcia@gmail.com>
2019-04-23 10:25:02 -0500

    Update main.c
    help

--- joe/main.c
+++ joe/main.c
@@ -6,17 +6,40 @@
 
 nio_console csl;
 
-static inline int wa_syscall(int nr)
-{
-	register int r0 asm("r0");
+static int match(device_t self) {
+  struct usb_attach_arg *uaa = device_get_ivars(self);
+  if (!uaa->iface) {
+    return UMATCH_NONE;
+  }
+  usbd_device_handle dev;
+  usbd_interface2device_handle(uaa->iface,&dev);
+  char* cp = malloc(1024);
+  usbd_devinfo(dev,1,cp);
+  fancy_puts(cp);
+  return UMATCH_NONE;
+}
 
-	asm volatile(
+static int (*methods[])(device_t) = {match, NULL, NULL, NULL};
+
+struct ulol_softc {
+	device_t sc_dev;
+	usbd_interface_handle sc_iface;
+	usbd_pipe_handle sc_intrpipe;
+	int sc_ep_addr;
+	struct s_usb_pipe_buf sc_ibuf;
+	int sc_isize;
+	int sc_enabled;
+};
+
+static inline int wa_syscall(int nr) {
+  //ughhh whyyyyyyyy... sad
+	register int r0 asm("r0");
+  asm volatile(
 		"swi %[nr]\n"
 		: "=r" (r0)
 		: [nr] "i" (nr)
 		: "memory", "r1", "r2", "r3", "r4", "r12", "lr");
-
-	return r0;
+  return r0;
 }
 
 int fancyputs(char *s) {
@@ -26,18 +49,18 @@ int fancyputs(char *s) {
 }
 
 int thing_register(void) {
-	fancyputs((char*)"Hello, World!");
+  usb_register_driver(2, methods, "ulol", 0, sizeof(struct ulol_softc));
 	return 0;
 }
 
 int main(void) {
 	assert_ndless_rev(801);
 	//nl_no_scr_redraw(); // is what I wish I could do
-    wa_syscall(e_nl_no_scr_redraw);
-	nio_init(&csl,NIO_MAX_COLS,NIO_MAX_ROWS,0,0,NIO_COLOR_WHITE,NIO_COLOR_BLACK,TRUE);
+	wa_syscall(e_nl_no_scr_redraw);
+	nio_init(&csl,NIO_MAX_COLS,2,0,0,NIO_COLOR_WHITE,NIO_COLOR_BLACK,TRUE);
 	nio_set_default(&csl);
 	nio_fflush(&csl);
 	thing_register();
-	nio_free(&csl);
+	nl_set_resident();
 	return 0;
 }
