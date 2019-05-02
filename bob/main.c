a7a28a800b3a73e0e29243929b9a12469b5afd63
Walter Mitty <gsitcia@gmail.com>
2019-05-01 21:54:57 -0500

    Update main.c

--- bob/main.c
+++ bob/main.c
@@ -1,11 +1,28 @@
 #include <libndls.h>
 #include <nspireio/nspireio.h>
+#include <stdint.h>
+
+int bob(void) {
+	return 42;
+}
+
+int fmem(nio_console *csl, void *ptr, int n) {
+	uint8_t *t = (uint8_t *) ptr;
+	for (int i = 0; i < n; n++) {
+		nio_fprintf(csl,"%02x ",*(ptr+i));
+		if (i%16==15) {
+			nio_fprintf(csl,"\n");
+		}
+	}
+	if (n%16 != 0) nio_fprintf(csl,"\n");
+	return 0;
+}
 
 int main(void) {
 	assert_ndless_rev(801);
 	nio_console csl;
 	nio_init(&csl,NIO_MAX_COLS,NIO_MAX_ROWS,0,0,NIO_COLOR_WHITE,NIO_COLOR_BLACK,1);
-	nio_fputs("Hello World!",&csl);
+	fmem(csl,(void *)bob,100);
 	nio_free(&csl);
 	wait_key_pressed();
 	return 0;
