dc6fd897b181165c11d46370f78d2ae6b4bb2e6b
Walter Mitty <gsitcia@gmail.com>
2019-05-03 04:13:37 +0000

    nothing special

--- bob/main.c
+++ bob/main.c
@@ -1,10 +1,6 @@
 #include <libndls.h>
 #include <nspireio/nspireio.h>
 
-int bob(void) {
-	return 0x42;
-}
-
 int fmem(nio_console *csl, void *ptr, int n) {
 	uint8_t *t = (uint8_t *) ptr;
 	for (int i = 0; i < n; i++) {
@@ -17,17 +13,32 @@ int fmem(nio_console *csl, void *ptr, int n) {
 	return 0;
 }
 
+int poke(int *ptr, int v) {
+	*ptr=v;
+	return 0;//that's it?
+}
+
 int main(void) {
 	assert_ndless_rev(801);
 	nio_console csl;
 	nio_init(&csl,NIO_MAX_COLS,NIO_MAX_ROWS,0,0,NIO_COLOR_WHITE,NIO_COLOR_BLACK,1);
-    nio_fprintf(&csl,"test%s\n","ing");
-	nio_fprintf(&csl,"An int is %d bytes or something\n",sizeof(int));
-    nio_fprintf(&csl,"Next we'll test the changing stuff thing\n");
-    wait_key_pressed();
-	fmem(&csl,(void *)bob,100);
-	nio_fprintf(&csl,"Address of bob: 0x%x",(unsigned*)bob);
+    int stop = 0;
+	while (!stop) {
+		char c;
+		unsigned int ptr,v;
+		nio_fscanf(&csl,"%c %i %i",&c,&ptr,&v);
+		switch (c) {
+			case 'e'://peek
+				fmem(&csl,(void *)ptr,v);
+				break;
+			case 'o'://poke
+				poke((void *)ptr,v);
+				break;
+			case 'q':
+				stop=1;
+				break;
+		}
+	}
 	nio_free(&csl);
-	wait_key_pressed();
 	return 0;
 }
---
+++ mainold.c
@@ -0,0 +1,33 @@
+#include <libndls.h>
+#include <nspireio/nspireio.h>
+
+int bob(void) {
+	return 0x42;
+}
+
+int fmem(nio_console *csl, void *ptr, int n) {
+	uint8_t *t = (uint8_t *) ptr;
+	for (int i = 0; i < n; i++) {
+		nio_fprintf(csl,"%02x ",*(t+i));
+		if (i%16==15) {
+			nio_fprintf(csl,"\n");
+		}
+	}
+	if (n%16 != 0) nio_fprintf(csl,"\n");
+	return 0;
+}
+
+int main(void) {
+	assert_ndless_rev(801);
+	nio_console csl;
+	nio_init(&csl,NIO_MAX_COLS,NIO_MAX_ROWS,0,0,NIO_COLOR_WHITE,NIO_COLOR_BLACK,1);
+    nio_fprintf(&csl,"test%s\n","ing");
+	nio_fprintf(&csl,"An int is %d bytes or something\n",sizeof(int));
+    nio_fprintf(&csl,"Next we'll test the changing stuff thing\n");
+    wait_key_pressed();
+	fmem(&csl,(void *)bob,100);
+	nio_fprintf(&csl,"Address of bob: 0x%x",(unsigned*)bob);
+	nio_free(&csl);
+	wait_key_pressed();
+	return 0;
+}
