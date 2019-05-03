f186b41693a2b2ddad7b3ab58717f0a3b83e8b95
Walter Mitty <gsitcia@gmail.com>
2019-05-03 12:39:36 +0000

    poopoo

--- bob/main.c
+++ bob/main.c
@@ -2,14 +2,15 @@
 #include <nspireio/nspireio.h>
 
 int fmem(nio_console *csl, void *ptr, int n) {
-	uint8_t *t = (uint8_t *) ptr;
+	nio_fprintf(csl,"Address: 0x%08x",ptr);
+	/*uint8_t *t = (uint8_t *) ptr;
 	for (int i = 0; i < n; i++) {
 		nio_fprintf(csl,"%02x ",*(t+i));
 		if (i%16==15) {
 			nio_fprintf(csl,"\n");
 		}
 	}
-	if (n%16 != 0) nio_fprintf(csl,"\n");
+	if (n%16 != 0) nio_fprintf(csl,"\n");*/
 	return 0;
 }
 
@@ -25,17 +26,18 @@ int main(void) {
     int stop = 0;
 	while (!stop) {
 		char c;
-		unsigned int ptr,v;
-		nio_fscanf(&csl,"%c %i %i",&c,&ptr,&v);
+		uint32_t ptr,v;
+		nio_fscanf(&csl,"%c %li %i",&c,&ptr,&v);
 		switch (c) {
+			case 'q':
+				stop=1;
+				nio_fprintf(&csl,"Quitting\n");
+				break;
 			case 'e'://peek
 				fmem(&csl,(void *)ptr,v);
 				break;
 			case 'o'://poke
 				poke((void *)ptr,v);
-				break;
-			case 'q':
-				stop=1;
 				break;
 		}
 	}
